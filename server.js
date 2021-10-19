8b30637b6f862dc4c729dc05f1f0b46433f8b510
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-18 23:26:23 -0500

    add heroku

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require('express');
 // instantiate the server
 const app = express();
 // requiring the db data 
-const {db} = require('./db/db.json');
+const {notes} = require('./db/db.json');
 
 
 
 
 
 //adding the get route
-app.get('/api/db', (req,res) => {
-    res.send('Hello World');
+app.get('/api/notes', (req,res) => {
+    //the res.send method is used to send short messages
+    // if we wanted to send big json data we would use the res.json method
+    res.json(notes);
 });
 
 // make the server listen 
